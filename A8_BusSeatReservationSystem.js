11bfa6853cd3d98a2364a89c5264c064ce956f6c
LenGamata <lengamata14@gmail.com>
2025-01-08 18:39:30 +0800

    Create A8_BusSeatReservationSystem.js

---
+++ A8_BusSeatReservationSystem.js
@@ -0,0 +1,5 @@
+def logout(self):
+    """Logs the ticket person out and returns to the initial menu."""
+    print("\nLogging out...")
+    # Additional cleanup or state management could be added here if needed
+    return False  # False indicates the user is logged out and the program should return to the main menu
